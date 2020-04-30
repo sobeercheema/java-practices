fdc35626e468cf188b86f460edd4a30f2faef4a0
sobeercheema <sobeercheema@gmail.com>
2020-04-30 17:54:11 +0530

    update new code commit

---
+++ src/sobs/javapractice/charBoolean.java
@@ -0,0 +1,19 @@
+package sobs.javapractice;
+
+/**
+ * Created by sobeersingh on 30/04/20.
+ */
+public class charBoolean {
+    public static void main(String [] args){
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+        System.out.println("My Char: " + myChar);
+        System.out.println("My unicode char: " + myUnicodeChar);
+        System.out.println("My copyright char: " +myCopyrightChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println("True boolean: " + myTrueBooleanValue);
+        System.out.println("My false boolean: " + myFalseBooleanValue);
+    }
+}
